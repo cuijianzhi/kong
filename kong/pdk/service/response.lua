cuijianzhi/kong

commit eb204989233b148397eb7751c1a58ed82123eaab

feat(pdk) implement the 'kong.service.response' kit

kong/pdk/service/response.lua
@@ -1,0 +1,184 @@
+---
+-- Manipulation of the response from the Service
+-- @module kong.service.response
+
+local phase_checker = require "kong.pdk.private.phases"
+
+
+local ngx = ngx
+local sub = string.sub
+local fmt = string.format
+local gsub = string.gsub
+local type = type
+local error = error
+local lower = string.lower
+local tonumber = tonumber
+local getmetatable = getmetatable
+local check_phase = phase_checker.check
+
+
+local PHASES = phase_checker.phases
+
+
+local header_body_log = phase_checker.new(PHASES.header_filter,
+                                          PHASES.body_filter,
+                                          PHASES.log)
+
+
+local function headers(response_headers, err)
+  local mt = getmetatable(response_headers)
+  local index = mt.__index
+  mt.__index = function(_, name)
+    if type(name) == "string" then
+      local var = fmt("upstream_http_%s", gsub(lower(name), "-", "_"))
+      if not ngx.var[var] then
+        return nil
+      end
+    end
+
+    return index(response_headers, name)
+  end
+
+  return response_headers, err
+end
+
+
+local function new(pdk, major_version)
+  local response = {}
+
+
+  local MIN_HEADERS            = 1
+  local MAX_HEADERS_DEFAULT    = 100
+  local MAX_HEADERS            = 1000
+
+
+  ---
+  -- Returns the HTTP status code of the response from the Service as a Lua number.
+  --
+  -- @function kong.service.response.get_status
+  -- @phases `header_filter`, `body_filter`, `log`
+  -- @treturn number|nil the status code from the response from the Service, or `nil`
+  -- if the request was not proxied (i.e. `kong.service.get_source()` returned
+  -- anything other than `"service"`.
+  -- @usage
+  -- kong.log.inspect(kong.service.response.get_status()) -- 418
+  function response.get_status()
+    check_phase(header_body_log)
+
+    return tonumber(sub(ngx.var.upstream_status or "", -3))
+  end
+
+
+  ---
+  -- Returns a Lua table holding the headers from the response from the Service. Keys are
+  -- header names. Values are either a string with the header value, or an array of
+  -- strings if a header was sent multiple times. Header names in this table are
+  -- case-insensitive and dashes (`-`) can be written as underscores (`_`); that is,
+  -- the header `X-Custom-Header` can also be retrieved as `x_custom_header`.
+  --
+  -- Unlike `kong.response.get_headers()`, this function will only return headers that
+  -- were present in the response from the Service (ignoring headers added by Kong itself).
+  -- If the request was not proxied to a Service (e.g. an authentication plugin rejected
+  -- a request and produced an HTTP 401 response), then the returned `headers` value
+  -- might be `nil`, since no response from the Service has been received.
+  --
+  -- By default, this function returns up to **100** headers. The optional
+  -- `max_headers` argument can be specified to customize this limit, but must be
+  -- greater than **1** and not greater than **1000**.
+  -- @function kong.service.response.get_headers
+  -- @phases `header_filter`, `body_filter`, `log`
+  -- @tparam[opt] number max_headers customize the headers to parse
+  -- @treturn table the response headers in table form
+  -- @treturn string err If more headers than `max_headers` were present, a
+  -- string with the error `"truncated"`.
+  -- @usage
+  -- -- Given a response with the following headers:
+  -- -- X-Custom-Header: bla
+  -- -- X-Another: foo bar
+  -- -- X-Another: baz
+  -- local headers = kong.service.response.get_headers()
+  -- if headers then
+  --   kong.log.inspect(headers.x_custom_header) -- "bla"
+  --   kong.log.inspect(headers.x_another[1])    -- "foo bar"
+  --   kong.log.inspect(headers["X-Another"][2]) -- "baz"
+  -- end
+  function response.get_headers(max_headers)
+    check_phase(header_body_log)
+
+    if max_headers == nil then
+      return headers(ngx.resp.get_headers(MAX_HEADERS_DEFAULT))
+    end
+
+    if type(max_headers) ~= "number" then
+      error("max_headers must be a number", 2)
+
+    elseif max_headers < MIN_HEADERS then
+      error("max_headers must be >= " .. MIN_HEADERS, 2)
+
+    elseif max_headers > MAX_HEADERS then
+      error("max_headers must be <= " .. MAX_HEADERS, 2)
+    end
+
+    return headers(ngx.resp.get_headers(max_headers))
+  end
+
+  ---
+  -- Returns the value of the specified response header.
+  --
+  -- Unlike `kong.response.get_header()`, this function will only return a header
+  -- if it was present in the response from the Service (ignoring headers added by Kong
+  -- itself).
+  --
+  -- @function kong.service.response.get_header
+  -- @phases `header_filter`, `body_filter`, `log`
+  -- @tparam string name The name of the header.
+  --
+  -- Header names in are case-insensitive and are normalized to lowercase, and
+  -- dashes (`-`) can be written as underscores (`_`); that is, the header
+  -- `X-Custom-Header` can also be retrieved as `x_custom_header`.
+  --
+  -- @treturn string|nil The value of the header, or `nil` if a header with
+  -- `name` was not found in the response. If a header with the same name is present
+  -- multiple times in the response, this function will return the value of the
+  -- first occurrence of this header.
+  -- @usage
+  -- -- Given a response with the following headers:
+  -- -- X-Custom-Header: bla
+  -- -- X-Another: foo bar
+  -- -- X-Another: baz
+  --
+  -- kong.log.inspect(kong.service.response.get_header("x-custom-header")) -- "bla"
+  -- kong.log.inspect(kong.service.response.get_header("X-Another"))       -- "foo bar"
+  function response.get_header(name)
+    check_phase(header_body_log)
+
+    if type(name) ~= "string" then
+      error("name must be a string", 2)
+    end
+
+    local header_value = response.get_headers()[name]
+    if type(header_value) == "table" then
+      return header_value[1]
+    end
+
+    return header_value
+  end
+
+
+  function response.get_raw_body()
+    -- TODO: implement
+  end
+
+
+  function response.get_body()
+    -- TODO: implement
+  end
+
+
+  return response
+end
+
+
+return {
+  new = new,
+}

kong-0.13.1-0.rockspec
@@ -161,6 +161,7 @@ build = {
     ["kong.pdk.ip"] = "kong/pdk/ip.lua",
     ["kong.pdk.log"] = "kong/pdk/log.lua",
     ["kong.pdk.service.request"] = "kong/pdk/service/request.lua",
+    ["kong.pdk.service.response"] = "kong/pdk/service/response.lua",
     ["kong.pdk.request"] = "kong/pdk/request.lua",
     ["kong.pdk.table"] = "kong/pdk/table.lua",
 

kong/pdk/init.lua
@@ -151,6 +151,7 @@ local MAJOR_VERSIONS = {
       "client",
       "request",
       "service.request",
+      "service.response",
     },
   },
 

t/01-pdk/07-service-response/00-phase_checks.t
@@ -1,0 +1,122 @@
+use strict;
+use warnings FATAL => 'all';
+use Test::Nginx::Socket::Lua;
+use t::Util;
+
+$ENV{TEST_NGINX_HTML_DIR} ||= html_dir();
+
+plan tests => repeat_each() * (blocks() * 2);
+
+run_tests();
+
+__DATA__
+
+=== TEST 1: verify phase checking in kong.service.response
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 200;
+        }
+    }
+
+    init_worker_by_lua_block {
+
+        phases = require("kong.pdk.private.phases").phases
+
+        phase_check_module = "service.response"
+        phase_check_data = {
+            {
+                method        = "get_status",
+                args          = {},
+                init_worker   = false,
+                certificate   = false,
+                rewrite       = "forced false",
+                access        = "forced false",
+                header_filter = true,
+                body_filter   = true,
+                log           = true,
+            }, {
+                method        = "get_headers",
+                args          = {},
+                init_worker   = false,
+                certificate   = false,
+                rewrite       = "forced false",
+                access        = "forced false",
+                header_filter = true,
+                body_filter   = true,
+                log           = true,
+            }, {
+                method        = "get_header",
+                args          = { "Host" },
+                init_worker   = false,
+                certificate   = false,
+                rewrite       = "forced false",
+                access        = "forced false",
+                header_filter = true,
+                body_filter   = true,
+                log           = true,
+            }, {
+                -- NYI, let everything pass
+                method        = "get_raw_body",
+                args          = {},
+                init_worker   = true,
+                certificate   = true,
+                rewrite       = true,
+                access        = true,
+                header_filter = true,
+                body_filter   = true,
+                log           = true,
+            }, {
+                -- NYI, let everything pass
+                method        = "get_body",
+                args          = {},
+                init_worker   = true,
+                certificate   = true,
+                rewrite       = true,
+                access        = true,
+                header_filter = true,
+                body_filter   = true,
+                log           = true,
+            },
+        }
+
+        phase_check_functions(phases.init_worker)
+    }
+
+    #ssl_certificate_by_lua_block {
+    #    phase_check_functions(phases.certificate)
+    #}
+}
+--- config
+    location /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        rewrite_by_lua_block {
+            phase_check_functions(phases.rewrite)
+        }
+
+        access_by_lua_block {
+            phase_check_functions(phases.access)
+        }
+
+        header_filter_by_lua_block {
+            phase_check_functions(phases.header_filter)
+        }
+
+        body_filter_by_lua_block {
+            phase_check_functions(phases.body_filter)
+        }
+
+        log_by_lua_block {
+            phase_check_functions(phases.log)
+        }
+    }
+--- request
+GET /t
+--- no_error_log
+[error]

t/01-pdk/07-service-response/01-get_status.t
@@ -1,0 +1,167 @@
+use strict;
+use warnings FATAL => 'all';
+use Test::Nginx::Socket::Lua;
+use t::Util;
+
+$ENV{TEST_NGINX_HTML_DIR} ||= html_dir();
+
+plan tests => repeat_each() * (blocks() * 3);
+
+run_tests();
+
+__DATA__
+
+=== TEST 1: service.response.get_status() returns a number
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 200;
+        }
+    }
+}
+--- config
+    location /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            ngx.arg[1] = "type: " .. type(pdk.service.response.get_status())
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+type: number
+--- no_error_log
+[error]
+
+
+
+=== TEST 2: service.response.get_status() returns 200
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 200;
+        }
+    }
+}
+--- config
+    location /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            ngx.arg[1] = "status: " .. pdk.service.response.get_status()
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+status: 200
+--- no_error_log
+[error]
+
+
+
+=== TEST 3: service.response.get_status() returns 404
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 404;
+        }
+    }
+}
+--- config
+    location /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            ngx.arg[1] = "status: " .. pdk.service.response.get_status()
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- error_code: 404
+--- response_body chop
+status: 404
+--- no_error_log
+[error]
+
+
+
+=== TEST 4: service.response.get_status() gets service status only
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 404;
+        }
+    }
+}
+--- config
+    location /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+            ngx.status = 200
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            ngx.arg[1] = "service response status: " .. pdk.service.response.get_status() .. "\n" ..
+                         "response status: " .. ngx.status
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- error_code: 200
+--- response_body chop
+service response status: 404
+response status: 200
+--- no_error_log
+[error]

t/01-pdk/07-service-response/02-get_headers.t
@@ -1,0 +1,473 @@
+use strict;
+use warnings FATAL => 'all';
+use Test::Nginx::Socket::Lua;
+use t::Util;
+
+$ENV{TEST_NGINX_HTML_DIR} ||= html_dir();
+
+plan tests => repeat_each() * (blocks() * 3);
+
+run_tests();
+
+__DATA__
+
+=== TEST 1: service.response.get_headers() returns a table
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 200;
+        }
+    }
+}
+--- config
+    location /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            ngx.arg[1] = "type: " .. type(pdk.service.response.get_headers())
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+type: table
+--- no_error_log
+[error]
+
+
+
+=== TEST 2: service.response.get_headers() returns service response headers
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            content_by_lua_block {
+                ngx.header.Foo = "Hello"
+                ngx.header.Bar = "World"
+                ngx.header.Accept = {
+                    "application/json",
+                    "text/html",
+                }
+            }
+        }
+    }
+}
+--- config
+    location = /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local headers = pdk.service.response.get_headers()
+
+            ngx.arg[1] = "Foo: " .. headers.Foo .. "\n" ..
+                         "Bar: " .. headers.Bar .. "\n" ..
+                         "Accept: " .. table.concat(headers.Accept, ", ")
+
+
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+Foo: Hello
+Bar: World
+Accept: application/json, text/html
+--- no_error_log
+[error]
+
+
+
+=== TEST 3: service.response.get_headers() returns service response headers with case-insensitive metatable
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            content_by_lua_block {
+                ngx.header["X-Foo-Header"] = "Hello"
+            }
+        }
+    }
+}
+--- config
+    location = /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local headers = pdk.service.response.get_headers()
+
+            ngx.arg[1] = "X-Foo-Header: " .. headers["X-Foo-Header"] .. "\n" ..
+                         "x-Foo-header: " .. headers["x-Foo-header"] .. "\n" ..
+                         "x_foo_header: " .. headers["x_foo_header"] .. "\n" ..
+                         "x_Foo_header: " .. headers["x_Foo_header"]
+
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+X-Foo-Header: Hello
+x-Foo-header: Hello
+x_foo_header: Hello
+x_Foo_header: Hello
+--- no_error_log
+[error]
+
+
+
+=== TEST 4: service.response.get_headers() fetches 100 headers max by default
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            default_type '';
+            content_by_lua_block {
+                for i = 1, 200 do
+                    ngx.header["X-Header-" .. i] = "test"
+                end
+            }
+        }
+    }
+}
+--- config
+    location = /t {
+        default_type '';
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local headers = pdk.service.response.get_headers()
+
+            local n = 0
+
+            for k in pairs(headers) do
+                n = n + 1
+            end
+
+            ngx.arg[1] = ngx.arg[1] .. "number of headers fetched: " .. n
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+number of headers fetched: 100
+--- no_error_log
+[error]
+
+
+
+=== TEST 5: service.response.get_headers() returns error when truncating
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            default_type '';
+            content_by_lua_block {
+                for i = 1, 200 do
+                    ngx.header["X-Header-" .. i] = "test"
+                end
+            }
+        }
+    }
+}
+--- config
+    location = /t {
+        default_type '';
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local headers, err = pdk.service.response.get_headers()
+            if err then
+            print("here: ", err)
+                ngx.arg[1] = err
+                ngx.arg[2] = true
+            end
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+truncated
+--- no_error_log
+[error]
+
+
+
+=== TEST 6: service.response.get_headers() fetches max_headers argument
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            default_type '';
+            content_by_lua_block {
+                for i = 1, 100 do
+                    ngx.header["X-Header-" .. i] = "test"
+                end
+            }
+        }
+    }
+}
+--- config
+    location = /t {
+        default_type  '';
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local headers = pdk.service.response.get_headers(60)
+
+            local n = 0
+
+            for k in pairs(headers) do
+                n = n + 1
+            end
+
+            ngx.arg[1] = ngx.arg[1] .. "number of headers fetched: " .. n
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+number of headers fetched: 60
+--- no_error_log
+[error]
+
+
+
+=== TEST 7: service.response.get_headers() raises error when trying to fetch with max_headers invalid value
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 200;
+        }
+    }
+}
+--- config
+    location = /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local _, err = pcall(pdk.service.response.get_headers, "invalid")
+
+            ngx.arg[1] = "error: " .. err
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+error: max_headers must be a number
+--- no_error_log
+[error]
+
+
+
+=== TEST 8: service.response.get_headers() raises error when trying to fetch with max_headers < 1
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 200;
+        }
+    }
+}
+--- config
+    location = /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local _, err = pcall(pdk.service.response.get_headers, 0)
+
+            ngx.arg[1] = "error: " .. err
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+error: max_headers must be >= 1
+--- no_error_log
+[error]
+
+
+
+=== TEST 9: service.response.get_headers() raises error when trying to fetch with max_headers > 1000
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            return 200;
+        }
+    }
+}
+--- config
+    location = /t {
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local _, err = pcall(pdk.service.response.get_headers, 1001)
+
+            ngx.arg[1] = "error: " .. err
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+error: max_headers must be <= 1000
+--- no_error_log
+[error]
+
+
+
+=== TEST 10: service.response.get_headers() returns only service headers
+--- http_config eval
+qq{
+    $t::Util::HttpConfig
+
+    server {
+        listen unix:$ENV{TEST_NGINX_HTML_DIR}/nginx.sock;
+
+        location / {
+            content_by_lua_block {
+                ngx.header["X-Service-Header"] = "test"
+            }
+        }
+    }
+}
+--- config
+    location = /t {
+        access_by_lua_block {
+            ngx.header["X-Non-Service-Header"] = "test"
+        }
+
+        proxy_pass http://unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+
+        header_filter_by_lua_block {
+            ngx.header.content_length = nil
+        }
+
+        body_filter_by_lua_block {
+            local PDK = require "kong.pdk"
+            local pdk = PDK.new()
+
+            local headers = pdk.service.response.get_headers()
+
+            ngx.arg[1] = "X-Service-Header: " .. headers["X-Service-Header"] .. "\n" ..
+                         "X-Non-Service-Header: " .. type(headers["X-Non-Service-Header"]) .. "\n" ..
+                         "X-Non-Service-Header: " .. ngx.header["X-Non-Service-Header"]
+
+            ngx.arg[2] = true
+        }
+    }
+--- request
+GET /t
+--- response_body chop
+X-Service-Header: test
+X-Non-Service-Header: nil
+X-Non-Service-Header: test
+--- no_error_log
+[error]

commit 3cac8ccf3a860986bb0ad990a3bd75317acf70a8

fix(pdk) prevent a memory leak with in 'service.response' module
Introduced in eb204989233b148397eb7751c1a58ed82123eaab, this index
metamethod attachment for response headers tables maintains a references
to itself. This was verified by increasing upvalues, functions, and
table object counts increasing when using this API over time.

The newer implementation is also more efficient and avoids creating
closures at runtime.

Fix #4143

kong/pdk/service/response.lua
@@ -25,10 +25,14 @@ local header_body_log = phase_checker.new(PHASES.header_filter,
                                           PHASES.log)
 
 
-local function headers(response_headers, err)
-  local mt = getmetatable(response_headers)
-  local index = mt.__index
-  mt.__index = function(_, name)
+local attach_mt_index
+
+
+do
+  local resp_headers_orig_mt_index
+
+
+  local function resp_headers_mt_index(t, name)
     if type(name) == "string" then
       local var = fmt("upstream_http_%s", gsub(lower(name), "-", "_"))
       if not ngx.var[var] then
@@ -36,10 +40,21 @@ local function headers(response_headers, err)
       end
     end
 
-    return index(response_headers, name)
+    return resp_headers_orig_mt_index(t, name)
   end
 
-  return response_headers, err
+
+  attach_mt_index = function(response_headers, err)
+    local mt = getmetatable(response_headers)
+
+    if not resp_headers_orig_mt_index then
+      resp_headers_orig_mt_index = mt.__index
+    end
+
+    mt.__index = resp_headers_mt_index
+
+    return response_headers, err
+  end
 end
 
 
@@ -106,7 +121,7 @@ local function new(pdk, major_version)
     check_phase(header_body_log)
 
     if max_headers == nil then
-      return headers(ngx.resp.get_headers(MAX_HEADERS_DEFAULT))
+      return attach_mt_index(ngx.resp.get_headers(MAX_HEADERS_DEFAULT))
     end
 
     if type(max_headers) ~= "number" then
@@ -119,7 +134,7 @@ local function new(pdk, major_version)
       error("max_headers must be <= " .. MAX_HEADERS, 2)
     end
 
-    return headers(ngx.resp.get_headers(max_headers))
+    return attach_mt_index(ngx.resp.get_headers(max_headers))
   end
 
   ---